7cf687246c9b606730210b533f1a3bf33739344f
박두현 <doubled4017@gmail.com>
2021-06-04 18:27:42 +0900

    oculus sdk add

---
+++ Assets/Scripts/TEst.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEst : MonoBehaviour
+{
+    CharacterController character;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+         
+    }
+
+}
